0a01a72f19d17002194f28d1fadc20a3e5e58819
Vlad <donatik919@gmail.com>
2024-06-26 14:15:16 +0300

    fix index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const http = require('http');
+const express = require('express')
+const app = module.exports = express()
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, Docker!\n');
+app.get('/', function(req, res){
+  res.send('Hello World');
 });
 
-server.listen(3000, '0.0.0.0', () => {
+app.listen(3000, '0.0.0.0', () => {
   console.log('Server running at http://0.0.0.0:3000/');
 });
